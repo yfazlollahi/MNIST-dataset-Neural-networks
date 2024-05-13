1ca4707d7fd0a9461dd1682ef0e4fe54c1f22e08
Yeganeh <yeganehfazlollahie@gmail.com>
2024-05-13 14:28:23 +0330

    Completed conv2d.py

--- mytorch/layer/conv2d.py
+++ mytorch/layer/conv2d.py
@@ -19,19 +19,48 @@ class Conv2d(Layer):
 
     def forward(self, x: Tensor) -> Tensor:
         "TODO: implement forward pass"
-        return ...
+        output_height = (x.shape[2] - self.kernel_size[0] + 2 * self.padding[0]) // self.stride[0] + 1
+        output_width = (x.shape[3] - self.kernel_size[1] + 2 * self.padding[1]) // self.stride[1] + 1
+
+        output = np.zeros((x.shape[0], self.out_channels, output_height, output_width))
+
+        for h in range(output_height):
+            for w in range(output_width):
+                h_start = h * self.stride[0]
+                h_end = h_start + self.kernel_size[0]
+                w_start = w * self.stride[1]
+                w_end = w_start + self.kernel_size[1]
+                output[:, :, h, w] = np.sum(x[:, :, h_start:h_end, w_start:w_end] * self.weight.data, axis=(2, 3))
+                if self.need_bias:
+                    output[:, :, h, w] += self.bias.data
+        return Tensor(output)
     
     def initialize(self):
         "TODO: initialize weights"
-        pass
+        self.weight = Tensor(
+            data=initializer((self.out_channels, self.in_channels, *self.kernel_size), mode=self.initialize_mode),
+            requires_grad=True
+        )
+
+        if self.need_bias:
+            self.bias = Tensor(
+                data=initializer((self.out_channels, 1, 1), mode="zeros"),
+                requires_grad=True
+            )
 
     def zero_grad(self):
         "TODO: implement zero grad"
-        pass
+        if self.weight:
+            self.weight.grad = None
+        if self.bias:
+            self.bias.grad = None
 
     def parameters(self):
         "TODO: return weights and bias"
-        return ...
+        params = [self.weight]
+        if self.need_bias:
+            params.append(self.bias)
+        return params
     
     def __str__(self) -> str:
         return "conv 2d - total params: {} - kernel: {}, stride: {}, padding: {}".format(
